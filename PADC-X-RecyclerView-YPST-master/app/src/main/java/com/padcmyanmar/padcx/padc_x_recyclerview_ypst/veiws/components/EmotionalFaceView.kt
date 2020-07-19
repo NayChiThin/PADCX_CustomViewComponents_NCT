4b302c6fd2aacf3a78f384ec60fe7f294898e5c2
NayChiThin <naychithin2000@gmail.com>
2020-07-19 15:23:56 +0630

    custom ui components part 6

--- PADC-X-RecyclerView-YPST-master/app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/components/EmotionalFaceView.kt
+++ PADC-X-RecyclerView-YPST-master/app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/components/EmotionalFaceView.kt
@@ -4,25 +4,55 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.withStyledAttributes
+import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.R
 
 class EmotionalFaceView(context: Context,attrs:AttributeSet):View(context,attrs) {
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    private var faceColor = Color.YELLOW
-    private var eyesColor = Color.BLACK
-    private var mouthColor = Color.BLACK
-    private var borderColor = Color.BLACK
+    private var faceColor = DEFAULT_FACE_COLOR
+    private var eyesColor = DEFAULT_EYES_COLOR
+    private var mouthColor = DEFAULT_MOUTH_COLOR
+    private var borderColor = DEFAULT_BORDER_COLOR
 
-    private var borderWidth = 4f
-    private var size = 320
+    private var borderWidth = DEFAULT_BORDER_WIDTH
+    private var size = 0
 
     private val mouthPath = Path()
+
+    var happinessState = HAPPY
+        set(state){
+            field = state
+            invalidate()
+        }
+
+    init {
+        paint.isAntiAlias = true
+        setUpAttributes(attrs)
+    }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         drawFaceBackground(canvas)
         drawEyes(canvas)
         drawMouth(canvas)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        size = measuredWidth.coerceAtMost(measuredHeight)
+        setMeasuredDimension(size,size)
+    }
+    private fun setUpAttributes(attrs: AttributeSet?) {
+        context.withStyledAttributes(attrs, R.styleable.EmotionalFaceView) {
+            happinessState = getString(R.styleable.EmotionalFaceView_state)?.toLongOrNull()?: HAPPY
+            faceColor = getColor(R.styleable.EmotionalFaceView_faceColor, DEFAULT_FACE_COLOR)
+            eyesColor = getColor(R.styleable.EmotionalFaceView_eyesColor, DEFAULT_EYES_COLOR)
+            mouthColor = getColor(R.styleable.EmotionalFaceView_mouthColor, DEFAULT_MOUTH_COLOR)
+            borderColor = getColor(R.styleable.EmotionalFaceView_borderColor, DEFAULT_BORDER_COLOR)
+            borderWidth = getDimension(R.styleable.EmotionalFaceView_borderWidth, DEFAULT_BORDER_WIDTH)
+        }
+
     }
     private fun drawFaceBackground(canvas: Canvas){
         paint.color = faceColor
@@ -40,11 +70,22 @@ class EmotionalFaceView(context: Context,attrs:AttributeSet):View(context,attrs)
 
     }
     private fun drawMouth(canvas: Canvas) {
+        mouthPath.reset()
+
         mouthPath.moveTo(size*0.22f,size*0.7f)
 
-        mouthPath.quadTo(size*0.5f,size*0.8f,size*0.78f,size*0.7f)
+        if(happinessState == HAPPY) {
 
-        mouthPath.quadTo(size*0.5f,size*0.9f,size*0.22f,size*0.7f)
+            mouthPath.quadTo(size*0.5f,size*0.8f,size*0.78f,size*0.7f)
+
+            mouthPath.quadTo(size*0.5f,size*0.9f,size*0.22f,size*0.7f)
+        }else {
+
+            mouthPath.quadTo(size*0.5f,size*0.5f,size*0.78f,size*0.7f)
+
+            mouthPath.quadTo(size*0.5f,size*0.6f,size*0.22f,size*0.7f)
+        }
+
 
         paint.color = mouthColor
         paint.style = Paint.Style.FILL
@@ -60,5 +101,15 @@ class EmotionalFaceView(context: Context,attrs:AttributeSet):View(context,attrs)
 
         val rightEye = RectF(size*0.57f,size*0.23f,size*0.68f,size*0.50f)
         canvas.drawOval(rightEye,paint)
+    }
+    companion object {
+        private const val DEFAULT_FACE_COLOR = Color.YELLOW
+        private const val DEFAULT_EYES_COLOR = Color.BLACK
+        private const val DEFAULT_MOUTH_COLOR = Color.BLACK
+        private const val DEFAULT_BORDER_COLOR = Color.BLACK
+        private const val DEFAULT_BORDER_WIDTH = 4.0f
+
+        const val HAPPY = 0L
+        const val SAD = 1L
     }
 }
